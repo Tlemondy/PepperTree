70e4ac2792328184c6fa0d9ddfa3919e0748dbf1
Reece Cussons <reececussons@gmail.com>
2024-03-13 23:04:26 +0200

    wip: file structure changed - packages updated

--- lib/pages/client/home/home_page.dart
+++ lib/pages/client/home/home_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:pepper_tree/utils/sizes_helper.dart';
 
 class HomePage extends ConsumerWidget {
