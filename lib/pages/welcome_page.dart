4392d2f6c876262b5644ee01e446940eca65536a
Reece Cussons <reececussons@gmail.com>
2024-03-13 20:19:22 +0200

    wip: go router init + riverpod

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class WelcomePage extends ConsumerWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Text('Pepper Tree'),
+          )
+        ],
+      ),
+    );
+  }
+}
